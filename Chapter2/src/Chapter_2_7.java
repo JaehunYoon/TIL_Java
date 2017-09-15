6dd1976a3b30ba80fcae3f0492f16ed56220598f
JaehunYoon <goodasd123@gmail.com>
2017-09-15 14:10:21 +0900

    TIL_Java : Studied how to use 'switch/case'

---
+++ Chapter2/src/Chapter_2_7.java
@@ -0,0 +1,29 @@
+
+public class Chapter_2_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char grade = 'C';
+		
+		switch(grade) {
+		case 'A':
+			System.out.println("참 잘했어요!");
+			break;
+		case 'B':
+			System.out.println("참 잘했어요!");
+			break;
+		case 'C':
+			System.out.println("잘했어요!");
+			break;
+		case 'D':
+			System.out.println("잘했어요!");
+			break;
+		case 'E':
+			System.out.println("집으로 가셈.");
+			break;
+		default:
+			System.out.println("잘못 입력했삼!");
+		}
+	}
+
+}
