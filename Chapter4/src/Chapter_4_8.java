dc35d60622c8cda503daf5c75a6d304eba723ffb
JaehunYoon <goodasd123@gmail.com>
2017-10-27 15:12:21 +0900

    TIL_Java : Learned about 'this'

---
+++ Chapter4/src/Chapter_4_8.java
@@ -0,0 +1,17 @@
+
+public class Chapter_4_8 {
+	String title;
+	String author;
+	int ISBN;
+	
+	public Chapter_4_8(String title, String author, int ISBN) {
+		this.title = title;
+		this.author = author;
+		this.ISBN = ISBN;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Chapter_4_8 javaBook = new Chapter_4_8("Java JDK","х╡╠Беб",3333);
+	}
+
+}
